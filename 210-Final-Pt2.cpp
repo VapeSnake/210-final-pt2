83c4833e1ccaceb5757d2ab51cd98cd3c2105d4d
Noel Mier-Luna <noel.lmier@gmail.com>
2026-05-21 16:42:16 -0700

    Milestone 1

--- 210-Final-Pt2.cpp
+++ 210-Final-Pt2.cpp
@@ -1,4 +1,4 @@
-//COMSC210 | Final Part 2 | Noel Mier-Luna
+// COMSC210 | Final Part 2 | Noel Mier-Luna
 #include <iostream>
 #include <string>
 #include <cstdlib>
@@ -7,31 +7,18 @@
 #include <array>
 using namespace std;
 
-vector<string> names = {
-        "Liam", "Olivia", "Noah", "Emma", "Oliver", 
-        "Ava", "Elijah", "Charlotte", "William", "Sophia",
-        "James", "Amelia", "Benjamin", "Isabella", "Lucas", 
-        "Mia", "Henry", "Evelyn", "Alexander", "Harper",
-        "Mason", "Camila", "Michael", "Gianna", "Ethan", 
-        "Abigail", "Daniel", "Luna", "Jacob", "Ella",
-        "Logan", "Elizabeth", "Jackson", "Sofia", "Levi", 
-        "Avery", "Sebastian", "Scarlett", "Jack", "Emily",
-        "Aiden", "Aria", "Owen", "Penelope", "Samuel", 
-        "Chloe", "Matthew", "Layla", "David", "Milani"
-    };
-
-    array<string, 10> drinks = {
-        "Espresso", "Latte", "Cappuccino", "Americano", "Mocha",
-        "Macchiato", "Flat White", "Cold Brew", "Iced Coffee", "Frappuccino"
-    };
 // This class was used in midterm 2, and I think it would be a great idea to use again for this coffee booth program.
-class CoffeeBooth {
-    private: struct Node {
+class CoffeeBooth
+{
+private:
+    struct Node
+    {
         string name;
         string drinkOrder;
-        Node * prev;
-        Node * next;
-        Node(string na, string dr, Node * p = nullptr, Node * n = nullptr) {
+        Node *prev;
+        Node *next;
+        Node(string na, string dr, Node *p = nullptr, Node *n = nullptr)
+        {
             name = na;
             drinkOrder = dr;
             prev = p;
@@ -39,212 +26,270 @@ class CoffeeBooth {
         }
     };
 
-    Node * head;
-    Node * tail;
+    Node *head;
+    Node *tail;
 
-    public: CoffeeBooth() {
+public:
+    CoffeeBooth()
+    {
         head = nullptr;
         tail = nullptr;
     }
 
-    void insert_after(string name, string drinkOrder, int position) {
-        if (position < 0) {
+    void insert_after(string name, string drinkOrder, int position)
+    {
+        if (position < 0)
+        {
             cout << "Position must be >= 0." << endl;
             return;
         }
 
-        Node * newNode = new Node(name, drinkOrder);
-        if (!head) {
+        Node *newNode = new Node(name, drinkOrder);
+        if (!head)
+        {
             head = tail = newNode;
             return;
         }
 
-        Node * temp = head;
+        Node *temp = head;
         for (int i = 0; i < position && temp; ++i)
-            temp = temp -> next;
+            temp = temp->next;
 
-        if (!temp) {
+        if (!temp)
+        {
             cout << "Position exceeds list size. Node not inserted.\n";
             delete newNode;
             return;
         }
 
-        newNode -> next = temp -> next;
-        newNode -> prev = temp;
-        if (temp -> next)
-            temp -> next -> prev = newNode;
+        newNode->next = temp->next;
+        newNode->prev = temp;
+        if (temp->next)
+            temp->next->prev = newNode;
         else
             tail = newNode;
-        temp -> next = newNode;
+        temp->next = newNode;
     }
 
-    void delete_val(string name) {
+    void delete_val(string name)
+    {
         if (!head)
             return;
 
-        Node * temp = head;
+        Node *temp = head;
 
-        while (temp && temp -> name != name)
-            temp = temp -> next;
+        while (temp && temp->name != name)
+            temp = temp->next;
 
         if (!temp)
             return;
 
-        if (temp -> prev)
-            temp -> prev -> next = temp -> next;
+        if (temp->prev)
+            temp->prev->next = temp->next;
         else
-            head = temp -> next;
+            head = temp->next;
 
-        if (temp -> next)
-            temp -> next -> prev = temp -> prev;
+        if (temp->next)
+            temp->next->prev = temp->prev;
         else
-            tail = temp -> prev;
+            tail = temp->prev;
 
         delete temp;
     }
 
-    void delete_pos(int pos) {
-        if (!head) {
+    void delete_pos(int pos)
+    {
+        if (!head)
+        {
             cout << "List is empty." << endl;
             return;
         }
 
-        if (pos == 1) {
+        if (pos == 1)
+        {
             pop_front();
             return;
         }
 
-        Node * temp = head;
+        Node *temp = head;
 
-        for (int i = 1; i < pos; i++) {
-            if (!temp) {
+        for (int i = 1; i < pos; i++)
+        {
+            if (!temp)
+            {
                 cout << "Position doesn't exist." << endl;
                 return;
-            } else
-                temp = temp -> next;
+            }
+            else
+                temp = temp->next;
         }
-        if (!temp) {
+        if (!temp)
+        {
             cout << "Position doesn't exist." << endl;
             return;
         }
 
-        if (!temp -> next) {
+        if (!temp->next)
+        {
             pop_back();
             return;
         }
 
-        Node * tempPrev = temp -> prev;
-        tempPrev -> next = temp -> next;
-        temp -> next -> prev = tempPrev;
+        Node *tempPrev = temp->prev;
+        tempPrev->next = temp->next;
+        temp->next->prev = tempPrev;
         delete temp;
     }
 
-    void push_back(string name, string drinkOrder) {
-        Node * newNode = new Node(name, drinkOrder);
+    void push_back(string name, string drinkOrder)
+    {
+        Node *newNode = new Node(name, drinkOrder);
         if (!tail)
             head = tail = newNode;
-        else {
-            tail -> next = newNode;
-            newNode -> prev = tail;
+        else
+        {
+            tail->next = newNode;
+            newNode->prev = tail;
             tail = newNode;
         }
     }
 
-    void push_front(string name, string drinkOrder) {
-        Node * newNode = new Node(name, drinkOrder);
+    void push_front(string name, string drinkOrder)
+    {
+        Node *newNode = new Node(name, drinkOrder);
         if (!head)
             head = tail = newNode;
-        else {
-            newNode -> next = head;
-            head -> prev = newNode;
+        else
+        {
+            newNode->next = head;
+            head->prev = newNode;
             head = newNode;
         }
     }
 
-    void pop_front() {
+    void pop_front()
+    {
 
-        if (!head) {
+        if (!head)
+        {
             cout << "List is empty." << endl;
             return;
         }
 
-        Node * temp = head;
+        Node *temp = head;
 
-        if (head -> next) {
-            head = head -> next;
-            head -> prev = nullptr;
-        } else
+        if (head->next)
+        {
+            head = head->next;
+            head->prev = nullptr;
+        }
+        else
             head = tail = nullptr;
         delete temp;
     }
 
-    void pop_back() {
-        if (!tail) {
+    void pop_back()
+    {
+        if (!tail)
+        {
             cout << "List is empty." << endl;
             return;
         }
-        Node * temp = tail;
+        Node *temp = tail;
 
-        if (tail -> prev) {
-            tail = tail -> prev;
-            tail -> next = nullptr;
-        } else
+        if (tail->prev)
+        {
+            tail = tail->prev;
+            tail->next = nullptr;
+        }
+        else
             head = tail = nullptr;
         delete temp;
     }
 
-    ~CoffeeBooth() {
-        while (head) {
-            Node * temp = head;
-            head = head -> next;
+    ~CoffeeBooth()
+    {
+        while (head)
+        {
+            Node *temp = head;
+            head = head->next;
             delete temp;
         }
     }
-    void print() {
-        Node * current = head;
-        if (!current) {
+    void print()
+    {
+        Node *current = head;
+        if (!current)
+        {
             cout << "List is empty." << endl;
             return;
         }
         cout << "resulting line: \n"; // Moved here to print before the loop to avoid printing it multiple times.
-        while (current) {
-            cout << current -> name << " \n";
-            current = current -> next;
+        while (current)
+        {
+            cout << current->name << " \n";
+            current = current->next;
         }
         cout << endl;
     }
 
-    void print_reverse() {
-        Node * current = tail;
-        if (!current) {
+    void print_reverse()
+    {
+        Node *current = tail;
+        if (!current)
+        {
             cout << "List is empty." << endl;
             return;
         }
-        while (current) {
-            cout << current -> name << " ";
-            current = current -> prev;
+        while (current)
+        {
+            cout << current->name << " ";
+            current = current->prev;
         }
         cout << endl;
     }
 
-    bool is_empty() {
+    bool is_empty()
+    {
         return head == nullptr;
     }
 
-    string get_head_name() {
+    string get_head_name()
+    {
         if (head)
-            return head -> name;
+            return head->name;
         return "";
     }
 
-    string get_head_drink() {
+    string get_head_drink()
+    {
         if (head)
-            return head -> drinkOrder;
+            return head->drinkOrder;
         return "";
     }
 };
 
-int main() {
+int main()
+{
+    vector<string> names = {
+        "Liam", "Olivia", "Noah", "Emma", "Oliver",
+        "Ava", "Elijah", "Charlotte", "William", "Sophia",
+        "James", "Amelia", "Benjamin", "Isabella", "Lucas",
+        "Mia", "Henry", "Evelyn", "Alexander", "Harper",
+        "Mason", "Camila", "Michael", "Gianna", "Ethan",
+        "Abigail", "Daniel", "Luna", "Jacob", "Ella",
+        "Logan", "Elizabeth", "Jackson", "Sofia", "Levi",
+        "Avery", "Sebastian", "Scarlett", "Jack", "Emily",
+        "Aiden", "Aria", "Owen", "Penelope", "Samuel",
+        "Chloe", "Matthew", "Layla", "David", "Milani"};
+
+    array<string, 10> drinks = {
+        "Espresso", "Latte", "Cappuccino", "Americano", "Mocha",
+        "Macchiato", "Flat White", "Cold Brew", "Iced Coffee", "Frappuccino"};
+
+    srand(time(0));
+    CoffeeBooth booth;
+    //Milestone 1 Complete. Vector for names and array for drinks created. Doubly Linked list imported from midterm 2 and changed to include drinks.
 
     return 0;
 }
